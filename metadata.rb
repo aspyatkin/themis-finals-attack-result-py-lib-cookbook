a33f50b17c0d5e9dd2d95c209cb76fc4b48f33be
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2017-04-22 17:21:31 +0400

    Update dependencies

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
 name 'themis-finals-attack-result-py-lib'
 description 'Installs and configures themis.finals.attack.result package'
-version '1.0.0'
+version '1.0.1'
 
 recipe 'themis-finals-attack-result-py-lib',
        'Installs and configures themis.finals.attack.result package'
-depends 'latest-git', '~> 1.1.11'
+depends 'git', '~> 6.0.0'
 depends 'git2', '~> 1.0.0'
-depends 'ssh-private-keys', '~> 1.0.0'
+depends 'ssh-private-keys', '~> 2.0.0'
+depends 'ssh_known_hosts', '~> 5.1.0'
