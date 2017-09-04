bf6f7a52bdb549dc82715b827da2bf48b9f4caf8
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2017-09-04 21:00:55 +0400

    Make use of instance cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,11 @@
 name 'themis-finals-attack-result-py-lib'
 description 'Installs and configures themis.finals.attack.result package'
-version '1.0.1'
+version '1.1.0'
 
 recipe 'themis-finals-attack-result-py-lib',
        'Installs and configures themis.finals.attack.result package'
-depends 'git', '~> 6.0.0'
+depends 'git', '~> 8.0.0'
 depends 'git2', '~> 1.0.0'
 depends 'ssh-private-keys', '~> 2.0.0'
-depends 'ssh_known_hosts', '~> 5.1.0'
+depends 'ssh_known_hosts', '~> 4.0.0'
+depends 'instance', '~> 1.0.0'
